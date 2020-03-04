61c0f6940b80e4c393e733097e10ba303792187f
Filbert <filbert@myboost.id>
2020-03-04 19:09:11 +0700

    change redis

--- app.js
+++ app.js
@@ -56,19 +56,19 @@ app.use((err, req, res, next) => {
 });
 
 
-if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// if (cluster.isMaster) {
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('online', (worker) => {
-    console.log(`Worker ${worker.process.pid} is online.`);
-  });
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died.`);
-  });
-} else {
-  app.listen(port, () => console.log(`Worker ${process.pid} is listening on port ${port}!`));
-}
+//   cluster.on('online', (worker) => {
+//     console.log(`Worker ${worker.process.pid} is online.`);
+//   });
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died.`);
+//   });
+// } else {
+//   app.listen(port, () => console.log(`Worker ${process.pid} is listening on port ${port}!`));
+// }
 
 module.exports = app;
