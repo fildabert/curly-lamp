2e83741535f83a22db5124804a36f57f398b50aa
Filbert <filbert@myboost.id>
2020-03-04 21:06:57 +0700

    va

--- controllers/transaction-controller.js
+++ controllers/transaction-controller.js
@@ -119,7 +119,7 @@ module.exports = {
       updatedPO.productId = checkProduct;
       if (!redisHelper.update('purchaseOrder', updatedPO, 'createdAt')) redisCache.del('purchaseOrder');
       if (!redisHelper.update('purchaseOrderSupplier', updatedPOSupplier, 'createdAt')) redisCache.del('purchaseOrderSupplier');
-      if (!redisHelper.update('transactions', transactionCreated, 'dateDelivered')) redisCache.del('transactions');
+      // if (!redisHelper.update('transactions', transactionCreated, 'dateDelivered')) redisCache.del('transactions');
 
       redisCache.del('products');
 
