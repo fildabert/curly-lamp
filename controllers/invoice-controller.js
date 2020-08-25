24667adfc1c570ebbd9cbfb8de58b9159ebbe88c
fildabert <filberttt@hotmail.com>
2020-08-25 12:34:36 +0700

    testuser5

--- controllers/invoice-controller.js
+++ controllers/invoice-controller.js
@@ -159,7 +159,7 @@ const updateInvoice = ({
   _id, customerId, topUpAmount,
 }) => new Promise(async (resolve, reject) => {
   try {
-    // 75,048,0001223345
+    // 75,048,00012233452
     const invoices = await Invoice.find({ customer: customerId }).sort({ createdAt: 'asc' });
 
     const cashFlows = await CashFlow.find({ customer: customerId }).sort({ dateIssued: 'asc' });
